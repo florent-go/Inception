2f84a46666e19ff5ff958110cd2d5faa90e58dd5
Florent Gomez <fgomez@c1r5p1.42nice.fr>
2021-12-28 16:32:03 +0100

    test

--- srcs/wordpress/wordpress.sh
+++ srcs/wordpress/wordpress.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/env bash
+
 #WORDPRESS
 wget https://wordpress.org/latest.tar.gz
 tar -xvf latest.tar.gz
 mv /wordpress/* /var/www/html/
-chown -R www-data:www-data /var/www/html
 rm -r /wordpress latest.tar.gz
 mv wp-config.php var/www/html/
+chown -R www-data:www-data /var/www/html
 
 wp core install --allow-root --path="/var/www/html" --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER	\
 		--admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email
@@ -12,6 +14,5 @@ wp core install --allow-root --path="/var/www/html" --url=$WP_URL --title=$WP_TI
 wp user create --allow-root --path="/var/www/html" --url=$WP_URL $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD
 
 
-
-exec php7.3
+exec "$@"
 #sleep infinity
